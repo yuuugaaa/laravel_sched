1e642d462987dd4888d77c3571c27cc86422e99b
yuuugaaa <yuuugaaa@gmail.com>
2023-04-09 16:03:40 +0000

    logo image setting

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import laravel, { refreshPaths } from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: true,
+    },
     plugins: [
         laravel({
             input: [
