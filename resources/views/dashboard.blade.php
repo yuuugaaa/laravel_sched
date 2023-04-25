7350523d16606c9614f2bf779439ad24a25834f0
yuuugaaa <yuuugaaa.321@gmail.com>
2023-04-25 03:54:42 +0000

    display calender

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,18 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            イベントカレンダー
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-8">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
+                <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+                    <div class="max-w-4xl mx-auto">
+                        @livewire('calender')
+                    </div>
+                </div>
             </div>
         </div>
     </div>
